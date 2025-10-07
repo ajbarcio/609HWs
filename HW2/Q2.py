2beb7cb66ebef1bc7276fe8d192498c4dd65730c
ajbarcio <ajbarcio@gmail.com>
2025-10-07 18:24:02 -0400

    did HW 2

---
+++ HW2/Q2.py
@@ -0,0 +1,131 @@
+import numpy as np
+
+np.set_printoptions(precision=8, suppress=True) 
+
+def LUD(A):
+    D = np.array(np.diag(np.diag(A)), dtype=float)
+    # print(D)
+    U = np.zeros_like(A, dtype=float)
+    for i in range(len(A)):
+        U += np.diag(np.diag(A, k=i), k=i)
+    U=U-D
+    # print(U)
+    L = np.zeros_like(A, dtype=float)
+    for i in range(len(A)):
+        L += np.diag(np.diag(A, k=-i), k=-i)
+    L=L-D
+    # print(L)
+    
+    return L, U, D
+
+A = np.array([[3,1,0],[1,3,1],[0,1,3]])
+b = np.array([4,5,4], dtype = float)
+
+if __name__=="__main__":
+
+    L, U, D = LUD(A)
+
+    D_inv = np.zeros_like(D, dtype=float)
+    for i in range(len(A)):
+        D_inv[i,i] = 1.0/float(D[i,i])
+
+    # print(D_inv)
+    B_J = -D_inv.dot(L+U)
+    print(B_J)
+    B_J_inf = np.linalg.norm(B_J, np.inf)
+    print(B_J_inf)
+    rho_B_J = np.max(np.linalg.eig(B_J)[0])
+    print(rho_B_J)
+
+    B_GS = -np.linalg.inv(L+D) @ U
+    print(B_GS)
+    B_GS_inf = np.linalg.norm(B_GS, np.inf)
+    print(B_GS_inf)
+    rho_B_GS = np.max(np.linalg.eig(B_GS)[0])
+    print(rho_B_GS)
+
+    omega_star = 2/(1+np.sqrt(1-rho_B_J**2))
+    print(omega_star)
+    print("for A sym. pos. def, omega should be between 0 and 2, and it is")
+
+
+    B_SOR = np.linalg.inv(omega_star*L + D) @ ((1-omega_star)*D - omega_star*U)
+    c_SOR = omega_star*np.linalg.inv(omega_star*L + D) @ b
+
+    x =   np.array([1,1,1])
+    x_0 = np.array([0,0,0])
+
+    def Jacobi(iters, x_0):
+        iters = iters+1
+        out = np.zeros([iters, 6])
+        out[0,0] = 0
+        out[0,1:4] = x_0
+        out[0,4]   = np.linalg.norm(x-x_0, np.inf)
+        out[0,5]   = np.linalg.norm(x-x_0, np.inf)
+        # print(out[0,:])
+        x_i = x_0
+        e_0 = np.linalg.norm(x-x_0, np.inf)
+        for i in range(iters):
+            out[i,0] = i
+            e_i = np.linalg.norm(x_i-x, np.inf)
+            out[i, 4] = e_i
+            out[i, 5] = e_i/e_0
+            if not i==iters-1:
+                x_next = (B_J @ x_i + D_inv @ b).flatten()
+                out[i+1,1:4] = x_next
+                x_i = x_next
+            e_0 = e_i
+        return out
+
+    def GaussSeidel(iters, x_0):
+        iters = iters+1
+        out = np.zeros([iters, 6])
+        out[0,0] = 0
+        out[0,1:4] = x_0
+        out[0,4]   = np.linalg.norm(x-x_0, np.inf)
+        out[0,5]   = np.linalg.norm(x-x_0, np.inf)
+        # print(out[0,:])
+        x_i = x_0
+        e_0 = np.linalg.norm(x-x_0, np.inf)
+        for i in range(iters):
+            out[i,0] = i
+            e_i = np.linalg.norm(x_i-x, np.inf)
+            out[i, 4] = e_i
+            out[i, 5] = e_i/e_0
+            if not i==iters-1:
+                x_next = (B_GS @ x_i + np.linalg.inv(L+D) @ b).flatten()
+                out[i+1,1:4] = x_next
+                x_i = x_next
+            e_0 = e_i
+        return out
+
+    def SOR(iters, x_0):
+        iters = iters+1
+        out = np.zeros([iters, 6])
+        out[0,0] = 0
+        out[0,1:4] = x_0
+        out[0,4]   = np.linalg.norm(x-x_0, np.inf)
+        out[0,5]   = np.linalg.norm(x-x_0, np.inf)
+        # print(out[0,:])
+        x_prev = x_0
+        x_i = np.zeros_like(x_0, dtype=float)
+        e_0 = np.linalg.norm(x-x_0, np.inf)
+        for i in range(iters):
+            out[i,0] = i
+            e_i = np.linalg.norm(x_i-x, np.inf)
+            out[i, 4] = e_i
+            out[i, 5] = e_i/e_0
+            if not i==iters-1:
+                x_next = (B_SOR @ x_i + c_SOR).flatten()
+                out[i+1,1:4] = x_next
+                x_i = x_next
+            e_0 = e_i
+        return out
+
+
+    J_table = Jacobi(10, x_0)
+    print(J_table)
+    GS_table = GaussSeidel(10, x_0)
+    print(GS_table)
+    SOR_table = SOR(10, x_0)
+    print(SOR_table)
---
+++ HW2/Q4.py
@@ -0,0 +1,81 @@
+import numpy as np
+from Q2 import LUD
+
+np.set_printoptions(precision=5, suppress=True) 
+
+threshold = 1e-2
+
+B = np.array(4*np.eye(3)+
+             np.diag(2*[-1], k=1)+
+             np.diag(2*[-1], k=-1), dtype=float)
+# print(B)
+A = np.array(np.block([[B,                -np.eye(3), np.zeros_like(B)],
+              [-np.eye(3),       B,          -np.eye(3)],
+              [np.zeros_like(B), -np.eye(3), B]]), dtype=float)
+# print(A)
+b = (0,0,1,0,0,1,0,0,1)
+L, U, D = LUD(A)
+# print(L)
+# print(U)
+# print(D)
+
+def JacobiStep(A: np.ndarray, b: np.ndarray, x: np.ndarray) -> np.ndarray:
+    xnew = np.zeros_like(x,dtype=np.float64)
+ 
+    for i in range(len(A)):
+        summation = 0
+        for j in range(len(A)):
+            if i == j:
+                continue
+            summation +=  A[i,j]*x[j]
+        xnew[i] = (b[i] - summation)/A[i,i]
+ 
+    return xnew
+
+def GaussSeidelStep(A: np.ndarray, b: np.ndarray, x: np.ndarray) -> np.ndarray:
+    # xnew = np.zeros_like(x,dtype=np.float64)
+    for i in range(len(A)):
+        summation = 0
+        for j in range(len(A)):
+            if i == j:
+                continue
+            summation +=  A[i,j]*x[j]
+        x[i] = (b[i] - summation)/A[i,i]
+    return x
+
+i = 0
+x_0 = np.zeros(len(A))
+res_norm = np.linalg.norm(A@x_0 - b)
+res_norm_prev = res_norm
+x_jacobi=x_0
+x_gaussS=x_0
+# print("")
+jacobi_out = []
+gaussS_out = []
+while res_norm > threshold:
+    if i < 100:
+        x_jacobi = JacobiStep(A, b, x_jacobi)
+        # print(i, end='\r')
+        res_norm = np.linalg.norm(A@x_jacobi - b)
+        jacobi_out.append(np.hstack([i, res_norm, res_norm/res_norm_prev]))
+        res_norm_prev = res_norm
+    i+=1
+print(x_jacobi)
+print(np.array(jacobi_out))
+
+i = 0
+res_norm = np.linalg.norm(A@x_0 - b)
+res_norm_prev = res_norm
+while res_norm > threshold:
+    if i < 100:
+        x_gaussS = GaussSeidelStep(A, b, x_gaussS)
+        # print(i, end='\r')
+        res_norm = np.linalg.norm(A@x_gaussS - b)
+        gaussS_out.append(np.hstack([i, res_norm, res_norm/res_norm_prev]))
+        res_norm_prev = res_norm
+    i+=1
+print(x_gaussS)
+print(np.array(gaussS_out))
+
+sol = np.linalg.solve(A, b)
+print(sol)
